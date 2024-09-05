8afd02bdd9ba3bc8813507d769815e8faa387dc3
Siddhi Sahu <himanisahu739@gmail.com>
2024-09-05 19:34:51 +0530

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/signup', (c) => {
+  return c.text("signup")
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text("signin")
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text("post blog")
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text("put blog")
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text("put blog")
+})
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("get bloggg")
+})
+export default app
